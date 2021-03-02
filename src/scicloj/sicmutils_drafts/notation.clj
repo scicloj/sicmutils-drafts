83c9569ccdbfdaefe55653f638b38cc104381924
daslu <daniel.slutsky@gmail.com>
2021-03-02 22:50:34 +0200

    added some commented-out example code

--- src/scicloj/sicmutils_drafts/notation.clj
+++ src/scicloj/sicmutils_drafts/notation.clj
@@ -1,6 +1,35 @@
 (ns scicloj.sicmutils-drafts.notation
-  (:require notespace.api
-            notespace-sicmutils.setup))
+  (:require [notespace.api :as notespace]
+            [notespace-sicmutils.setup]
+            [scicloj.sicmutils-drafts.utils]))
+
+^kind/hidden
+(comment
+  ;; Here is how you can use Notespace in a minimal way to render this namespace. Please keep these calls inside a comment block.
+  ;; Notespace does offer a mode dynamic experience, with some integration into popular Clojure editors' functions.
+  ;; See the Notespace tutorial:
+  ;; https://scicloj.github.io/notespace/doc/notespace/v3-experiment1-test/index.html
+  ;; A screencast is coming soon.
+
+  ;; Initialize notespace:
+  (notespace/init)
+
+  ;; Alternatively, Initialize notespace and open the browser view:
+  (notespace/init-with-browser)
+
+  ;; Evauating all notes in this namespace, and updating the browser view:
+  (notespace/eval-this-notespace)
+
+  ;; Notespace does have bugs.
+  ;; If the browser seems out-of-sync, you can try to refresh it.
+  ;; If the state still seems problematic, you can init again.
+
+  ;; Rendering current browser view into a static html file (under the `docs` directory):
+  (notespace/render-static-html)
+
+  ;; Printing the appropriate link that should be added to the Table of Contents (`toc.md`):
+  (println (scicloj.sicmutils-drafts.utils/ns->toc-link "notation")))
+
 
 ["# Appendix: Our Notation
 
